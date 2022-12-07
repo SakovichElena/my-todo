fa9462840704c9593cd7c21863c0b2867952a91a
SakovichElena <lenkasakovich.le@gmail.com>
2022-12-08 02:24:57 +0300

    add todo-item

--- dist/index.c36f364e.js
+++ dist/index.c36f364e.js
@@ -1,3 +1,4 @@
+"use strict";
 // const todo = {
 //     action(e) {},
 //     add() {},
@@ -9,9 +10,10 @@
 const form = document.querySelector("#form");
 const input_add = document.querySelector("#input_add");
 const list_group = document.querySelector("#list_group");
-console.log(list_group);
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
+const btn_add = document.querySelector("#add");
+form.addEventListener("submit", addTask);
+function addTask(e) {
+    e.preventDefault();
     const taskText = input_add.value;
     console.log(taskText);
     const taskHTML = `<li class="list-group-item">
@@ -22,10 +24,19 @@ form.addEventListener("submit", function(event) {
         <input type="date" class="input-date" value="2022-12-07">
     </div>
     </li>`;
-    console.log(taskHTML);
+    // console.log(taskHTML);
+    // function addEl () {
+    //     btn_add.addEventListener("click", addListElement);
+    // }
+    // 
+    btn_add.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log(taskHTML);
+    });
+    //  
     list_group.insertAdjacentHTML("beforeend", taskHTML);
     input_add.value = "";
     input_add.focus();
-});
+}
 
 //# sourceMappingURL=index.c36f364e.js.map
